8e3aeb5426f1949a247b766506071d27198b8093
anajaracamposdesousa <152650846+anajaracamposdesousa@users.noreply.github.com>
2024-04-02 16:42:10 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,9 @@
+user={
+    'name':"Laura",
+    'photo':"https://www.fiquediva.com.br/-/media/project/loreal/brand-sites/fiquediva/usa/pt-br/article-image-fd/article-latest/cabelo-crespo-curto-5-ideias-de-cortes-para-rosto-redondo.jpg",
+    'idade': "17",
+    'pai': "Marcelo",
+    'mae': "Aurora",
+    'sexo': "Feminino",
+    'sobremim': "Tenho 17 anos, sou dedicada e focada."
+}
